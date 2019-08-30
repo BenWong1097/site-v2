488d84efbf676db7f3bb228024893de97bd2b2af
Benjamin Wong <benjaminwong@tamu.edu>
2019-08-30 12:01:34 -0500

    Implemented Footer section. Closes #5.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import About from './components/About';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Experience/>
       <Projects/>
       <Contact/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Footer(props){
+    return <div className="bg-dark footer faded">
+        Built with lotsa ♥ and no <span aria-label="coffee" role="img">☕</span> by Ben Wong.
+    </div>
+};
